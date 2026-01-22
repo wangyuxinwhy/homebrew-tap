eeaa01b9ea3a258f29534a1735f60b19faaa918d
wangyuxin <wangyuxin@mokahr.com>
2026-01-23 00:03:15 +0800

    feat: bump catsay to 1.0.1

--- Formula/catsay.rb
+++ Formula/catsay.rb
@@ -1,9 +1,9 @@
 class Catsay < Formula
   desc "A cat that says things in your terminal"
   homepage "https://github.com/wangyuxinwhy/catsay"
-  url "https://github.com/wangyuxinwhy/catsay/releases/download/v1.0.0/catsay"
-  sha256 "bee029d7ff9624dfc65e575d6703076332b16f65076700b94fe0ff7384fbc602"
-  version "1.0.0"
+  url "https://github.com/wangyuxinwhy/catsay/releases/download/v1.0.1/catsay"
+  sha256 "96e75ea21cc6f276d0b075ae8d0fb98c0686e17ecc512ccdb7d20199d2c8bfd2"
+  version "1.0.1"
   license "MIT"
 
   def install
